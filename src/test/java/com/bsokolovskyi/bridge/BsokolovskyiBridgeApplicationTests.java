2be89e493f9da634ab5f18299294b8d2f3b1cd4b
Bohdan Sokolovskyi <sokol.chemist@gmail.com>
2022-06-15 02:45:35 +0300

    add startup file, add project configs

---
+++ src/test/java/com/bsokolovskyi/bridge/BsokolovskyiBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsokolovskyi.bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsokolovskyiBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
